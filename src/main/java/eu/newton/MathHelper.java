bc3174753cabb5ea9e1583d2904c2cc47d6c8b5a
MuffinOverlord <37265360+MuffinOverlord@users.noreply.github.com>
2018-08-25 12:09:43 +0200

    Rework Math to get rid of BigDecimal

---
+++ src/main/java/eu/newton/MathHelper.java
@@ -0,0 +1,20 @@
+package eu.newton;
+
+public final class MathHelper {
+
+    public static double add(double a, double b) {
+        int ad = (int) ((a - ((int) a)) * 10000);
+        int bd = (int) ((b - ((int) b)) * 10000);
+        return (((int) a) + ((int) b)) + (((double) (ad + bd)) / 10000);
+    }
+
+    public static double minus(double a, double b) {
+        int ad = (int) ((a - ((int) a)) * 10000);
+        int bd = (int) ((b - ((int) b)) * 10000);
+        return (((int) a) - ((int) b)) + (((double) (ad - bd)) / 10000);
+    }
+
+
+
+
+}
