a18acba81b98ed80a5a0eb524096e0aadc7cd473
MuffinOverlord <37265360+MuffinOverlord@users.noreply.github.com>
2018-08-26 11:56:55 +0200

    Fix tests

--- src/test/java/ParserAndEvaluationTest.java
+++ src/test/java/ParserAndEvaluationTest.java
@@ -2,20 +2,17 @@ import eu.newton.MathFunction;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import javax.script.ScriptException;
-import java.math.BigDecimal;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserAndEvaluationTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "functions.csv", numLinesToSkip = 2)
-    void testFunction(String function, double expected) throws ScriptException {
+    void testFunction(String function, double expected) throws Exception {
         MathFunction f = new MathFunction(function);
-        BigDecimal result = f.evaluate(BigDecimal.valueOf(2));
+        double result = f.evaluate(2.0);
 
-        assertEquals(expected, result.doubleValue());
+        assertEquals(((double) ((int) (expected * 1000))) / 1000, ((double) ((int) (result * 1000))) / 1000);
         System.out.println(result);
 
     }
