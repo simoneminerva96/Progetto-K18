1f36bf5ef01c1206bc4eae716fe0c4f313276f7b
MuffinOverlord <37265360+MuffinOverlord@users.noreply.github.com>
2018-08-27 15:42:51 +0200

    Implement Zero and Extrema

--- src/main/java/eu/newton/MathFunction.java
+++ src/main/java/eu/newton/MathFunction.java
@@ -1,6 +1,8 @@
 package eu.newton;
 
 import eu.newton.api.IDoubleDifferentiable;
+import eu.newton.api.IDoubleExtrema;
+import eu.newton.api.IDoubleZero;
 import eu.newton.magic.exceptions.LambdaCreationException;
 import eu.newton.parser.FunctionParser;
 import org.apache.logging.log4j.LogManager;
@@ -8,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.function.DoubleUnaryOperator;
 
-public final class MathFunction implements IDoubleDifferentiable {
+public final class MathFunction implements IDoubleDifferentiable, IDoubleExtrema, IDoubleZero {
 
     private static final Logger logger = LogManager.getLogger(MathFunction.class);
     
@@ -34,5 +36,76 @@ public final class MathFunction implements IDoubleDifferentiable {
     @Override
     public double evaluate(double x) {
         return this.f.applyAsDouble(x);
+    }
+
+    @Override
+    public double getMaximum(double a, double b) {
+
+        double x = a * 100;
+        double y = evaluate(x / 100);
+
+        double max = x / 100;
+        double temp = y;
+
+        double top = b * 100;
+
+        x += 1;
+        while (x <= top) {
+            double value = x / 100;
+            y = evaluate(value);
+            if (y > temp) {
+                temp = y;
+                max = value;
+            }
+
+            x += 1;
+        }
+
+        return max;
+    }
+
+    @Override
+    public double getMinimum(double a, double b) {
+        double x = a * 100;
+        double y = evaluate(x / 100);
+
+        double min = x / 100;
+        double temp = y;
+
+        double top = b * 100;
+
+        x += 1;
+        while (x <= top) {
+            double value = x / 100;
+            y = evaluate(value);
+            if (y < temp) {
+                temp = y;
+                min = value;
+            }
+
+            x += 1;
+        }
+
+        return min;
+    }
+
+    @Override
+    public double getZero(double a, double b) {
+        double x = a * 100;
+        double y;
+
+        double top = b * 100;
+
+        while (x <= top) {
+            double value = x / 100;
+            y = evaluate(value);
+            if (y == 0) {
+                return value;
+            }
+            x += 1;
+        }
+
+        return Double.NaN;
+
     }
 }
